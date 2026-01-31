7a7fde8d57d9345ea5de7262c0a8f9ab8fdb3f32
ShivamVerma380 <shivamvermasv380@gmail.com>
2026-01-31 10:44:15 +0530

    Backend project structure

---
+++ backend/edupath/src/main/java/com/aiboomi/edupath/EdupathApplication.java
@@ -0,0 +1,13 @@
+package com.aiboomi.edupath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupathApplication.class, args);
+	}
+
+}
